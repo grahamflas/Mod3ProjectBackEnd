89a99082549e2eb50ff6689d00d4b294e44089fc
Graham Flaspoehler <grahamflas@gmail.com>
2019-11-13 14:20:10 -0500

    Change users#show to find user using GET params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find_by(params[:username])
+    user = User.find(params[:id])
     render json: user, include: [:games]
   end
 
